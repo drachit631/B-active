01e6d425051e495b4b548c6fecdb825185a2307e
drachit631 <drachit631@gmail.com>
2022-01-05 18:23:33 +0530

    restructuring 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
-import Footer from './components/Footer';
-import Homepage from './components/Homepage';
-import Mainrouter from './components/Mainrouter';
-import './App.css';
 import Userprofile from './components/Userprofile';
+import './App.css';
 
 function App() {
   return (
